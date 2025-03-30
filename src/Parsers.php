f35b75c21b9b89ca199b91280ecd6e1d04772b38
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-30 17:19:16 +0300

    update Parsers.php
    change switch to match in parse method

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,30 +9,19 @@ class Parsers
     private function getFileContent(string $pathToFile): string
     {
         $content = is_file($pathToFile) ? file_get_contents($pathToFile) : exit("File '$pathToFile' not found.\n");
-
         return $content;
     }
 
-    public function parse(string $pathToFile): array
+    public function parse(string $pathToFile): array|string
     {
         $content = $this->getFileContent($pathToFile);
         $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
 
-        switch ($extension) {
-            case 'json':
-                $parsedFile = json_decode($content, true);
-                break;
-            case 'yml':
-            case 'yaml':
-                $parsedFile = Yaml::parse($content);
-                break;
-            default:
-                exit("\nUnsupported format '$extension' of incoming file.\n");
-        }
-
-        echo "\nParsed Content:\n";
-        var_export($parsedFile);
-        echo "\n";
+        $parsedFile = match ($extension) {
+            'json' => json_decode($content, true),
+            'yml', 'yaml' => Yaml::parse($content),
+            default => exit("\nUnsupported format '$extension' of incoming file.\n"),
+        };
 
         return $parsedFile;
     }
