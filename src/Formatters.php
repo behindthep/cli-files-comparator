38bd5948120ae42ca44ed3b15dd42fdb59ce3f49
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-30 17:22:28 +0300

    add format to method

--- src/Formatters.php
+++ src/Formatters.php
@@ -2,11 +2,19 @@
 
 namespace Diff\Comparator;
 
+use Diff\Comparator\Formatters\Stylish;
+
 class Formatters
 {
-    public function makeFormat(string $difference, string $format): string
+    public function makeFormat(array $difference, string $format): string
     {
-        $result = "{\n$difference}\n";
-        return $result;
+        switch ($format) {
+            case 'stylish':
+                $stylish = new Stylish();
+                $formatted = $stylish->stylishFormat($difference);
+                return $formatted;
+            default:
+                exit("Unknown format '$format'.\n");
+        }
     }
 }
