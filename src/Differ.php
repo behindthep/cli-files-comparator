e667491c7118ec118d19ed588172d8ad4cd556ec
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-30 17:25:10 +0300

    change method for nested logic

--- src/Differ.php
+++ src/Differ.php
@@ -17,52 +17,80 @@ class Differ
 
         $formatter = new Formatters();
         $result = $formatter->makeFormat($diff, $format);
-    
+
         return $result;
     }
 
-    private function makeDiff(array $parsedFirstFile, array $parsedSecondFile): string
+    private function makeDiff(array $parsedFirstFile, array $parsedSecondFile): array
     {
-        $uniqueKeys = array_unique(array_merge(array_keys($parsedFirstFile), array_keys($parsedSecondFile)));
+        $uniqueKeys = $this->getSortedUniqueKeys($parsedFirstFile, $parsedSecondFile);
 
-        sort($uniqueKeys);
-
-        $difference = '';
+        $differ = [];
 
         foreach ($uniqueKeys as $key) {
             $firstValue  = $parsedFirstFile[$key]  ?? null;
             $secondValue = $parsedSecondFile[$key] ?? null;
 
-            if (array_key_exists($key, $parsedFirstFile) && array_key_exists($key, $parsedSecondFile)) {
-                $firstData  = $this->stringifyValue($firstValue);
-                $secondData = $this->stringifyValue($secondValue);
-
-                if ($firstData === $secondData) {
-                    $difference .= "    $key: $firstData\n";
-                } else {
-                    $difference .= "  - $key: $firstData\n  + $key: $secondData\n";
-                }
-            } elseif (array_key_exists($key, $parsedFirstFile) && !array_key_exists($key, $parsedSecondFile)) {
-                $firstData   = $this->stringifyValue($firstValue);
-                $difference .= "  - $key: $firstData\n";
-            } elseif (!array_key_exists($key, $parsedFirstFile) && array_key_exists($key, $parsedSecondFile)) {
-                $secondData  = $this->stringifyValue($secondValue);
-                $difference .= "  + $key: $secondData\n";
+            if (is_array($firstValue) && is_array($secondValue)) {
+                $differ[] = [
+                    'status' => 'nested',
+                    'key'    => $key,
+                    'value1' => $this->makeDiff($firstValue, $secondValue),
+                    'value2' => null
+                ];
+                continue;
+            }
+            if (!array_key_exists($key, $parsedFirstFile)) {
+                $differ[] = [
+                    'status' => 'added',
+                    'key'    => $key,
+                    'value1' => $secondValue,
+                    'value2' => null
+                ];
+                continue;
+            }
+            if (!array_key_exists($key, $parsedSecondFile)) {
+                $differ[] = [
+                    'status' => 'removed',
+                    'key'    => $key,
+                    'value1' => $firstValue,
+                    'value2' => null
+                ];
+                continue;
+            }
+            if ($firstValue === $secondValue) {
+                $differ[] = [
+                    'status' => 'unchanged',
+                    'key'    => $key,
+                    'value1' => $firstValue,
+                    'value2' => $firstValue
+                ];
+                continue;
+            }
+            if ($firstValue !== $secondValue) {
+                $differ[] = [
+                    'status' => 'updated',
+                    'key'    => $key,
+                    'value1' => $firstValue,
+                    'value2' => $secondValue
+                ];
+                continue;
             }
         }
 
-        return $difference;
+        return $differ;
     }
 
-    private function stringifyValue(mixed $value): mixed
+    private function getSortedUniqueKeys(array $parsedFirstFile, array $parsedSecondFile): array
     {
-        if (is_bool($value)) {
-            return $value ? 'true' : 'false';
-        } 
-        elseif (is_null($value)) {
-            return 'null';
-        }
+        $firstFileKeys  = array_keys($parsedFirstFile);
+        $secondFileKeys = array_keys($parsedSecondFile);
 
-        return $value;
+        $filesKeys  = array_merge($firstFileKeys, $secondFileKeys);
+        $uniqueKeys = array_unique($filesKeys);
+
+        sort($uniqueKeys);
+
+        return $uniqueKeys;
     }
 }
