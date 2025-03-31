45d78ec3615645c37125f72f39c2bf7f2c369f50
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-31 17:35:30 +0300

    update sort keys function

--- src/Differ.php
+++ src/Differ.php
@@ -2,6 +2,8 @@
 
 namespace Diff\Comparator;
 
+use function Functional\sort;
+
 class Differ
 {
     public static function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
@@ -65,48 +67,17 @@ class Differ
         }
     }
 
-        foreach ($uniqueKeys as $key) {
-            $firstValue  = $parsedFirstFile[$key]  ?? null;
-            $secondValue = $parsedSecondFile[$key] ?? null;
+    private static function getSortedUniqueKeys(array $associativeArr1, array $associativeArr2): array
+    {
+        $keys1 = array_keys($associativeArr1);
+        $keys2 = array_keys($associativeArr2);
 
-            if (is_array($firstValue) && is_array($secondValue)) {
-                $difference[] = [
-                    'status' => 'nested',
-                    'key'    => $key,
-                    'value1' => self::makeDiff($firstValue, $secondValue),
-                    'value2' => null
-                ];
-            } elseif (!array_key_exists($key, $parsedFirstFile)) {
-                $difference[] = [
-                    'status' => 'added',
-                    'key'    => $key,
-                    'value1' => $secondValue,
-                    'value2' => null
-                ];
-            } elseif (!array_key_exists($key, $parsedSecondFile)) {
-                $difference[] = [
-                    'status' => 'removed',
-                    'key'    => $key,
-                    'value1' => $firstValue,
-                    'value2' => null
-                ];
-            } elseif ($firstValue === $secondValue) {
-                $difference[] = [
-                    'status' => 'unchanged',
-                    'key'    => $key,
-                    'value1' => $firstValue,
-                    'value2' => $secondValue
-                ];
-            } elseif ($firstValue !== $secondValue) {
-                $difference[] = [
-                    'status' => 'updated',
-                    'key'    => $key,
-                    'value1' => $firstValue,
-                    'value2' => $secondValue
-                ];
-            }
-        }
+        $combinedKeys = array_merge($keys1, $keys2);
+        $uniqueKeys   = array_unique($combinedKeys);
 
-        return $difference;
+        /**
+         *
+         */
+        return sort($uniqueKeys, fn($left, $right) => strcmp($left, $right));
     }
 }
