1975402255ebcdb52e3ee5225eaaae9a3b77d545
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-31 13:17:16 +0300

    add paddings

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,19 +8,22 @@ class Parsers
 {
     private static function getFileContent(string $pathToFile): string
     {
-        $content = is_file($pathToFile) ? file_get_contents($pathToFile) : exit("File '$pathToFile' not found.\n");
+        $content = is_file($pathToFile)
+        ? file_get_contents($pathToFile)
+        : exit("File '$pathToFile' not found.\n");
+
         return $content;
     }
 
     public static function parse(string $pathToFile): array|string
     {
-        $content = self::getFileContent($pathToFile);
+        $content   = self::getFileContent($pathToFile);
         $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
 
         $parsedFile = match ($extension) {
-            'json' => json_decode($content, true),
+            'json'        => json_decode($content, true),
             'yml', 'yaml' => Yaml::parse($content),
-            default => exit("\nUnsupported format '$extension' of incoming file.\n"),
+            default       => exit("Unsupported format '$extension' of incoming file.\n"),
         };
 
         return $parsedFile;
