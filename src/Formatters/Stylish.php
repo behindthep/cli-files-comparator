cf47b0cc575f20ed5844ddb5540564fed45ed935
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-03-31 13:19:12 +0300

    fix visuality

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -4,20 +4,56 @@ namespace Diff\Comparator\Formatters;
 
 class Stylish
 {
-    public static function stylishFormat(array $diff): string
+    public static function stylishFormat(array $difference): string
     {
-        $formattedDiff = self::makeStringFromDiff($diff);
-        $result = implode("\n", $formattedDiff);
-        return "{\n$result\n}\n";
+        $formattedDiff   = self::makeStringFromDiff($difference);
+        $stringifiedDiff = implode("\n", $formattedDiff);
+
+        return "{\n$stringifiedDiff\n}\n";
     }
 
-    private static function makeStringFromDiff(array $diff, int $level = 0): array
+    private static function getSpaces(int $numberOfRepetitions): string
     {
-        $stringifiedDiff = [];
-        $spaces = self::getSpaces($level);
+        return str_repeat('    ', $numberOfRepetitions);
+    }
+
+    private static function stringifyValue(mixed $value, int $level): mixed
+    {
+        switch (true) {
+            case is_null($value):
+                return 'null';
+            case is_bool($value):
+                return $value ? 'true' : 'false';
+            case is_array($value):
+                $stringifiedArr = self::convertArrayToString($value, $level);
+                $spaces         = self::getSpaces($level);
+                return "{$stringifiedArr}{$spaces}";
+            default:
+                return "$value";
+        }
+    }
+
+    private static function convertArrayToString(array $arr, int $level): string
+    {
         $nextLevel = $level + 1;
 
-        foreach ($diff as $node) {
+        $formattedArr = array_map(function ($key) use ($arr, $nextLevel) {
+            $value  = self::stringifyValue($arr[$key], $nextLevel);
+            $spaces = self::getSpaces($nextLevel);
+            return "\n{$spaces}{$key}: $value";
+        }, array_keys($arr));
+
+        return implode('', $formattedArr);
+    }
+
+    // по названию должна возвращять строку а не массив. можно объединить эту и stylishFormat
+    private static function makeStringFromDiff(array $difference, int $level = 0): array
+    {
+        $stringifiedDiff = [];
+        $nextLevel       = $level + 1;
+        $spaces          = self::getSpaces($level);
+
+        foreach ($difference as $node) {
             $status = $node['status'];
             $key    = $node['key'];
             $value1 = $node['value1'];
@@ -25,8 +61,8 @@ class Stylish
 
             switch ($status) {
                 case 'nested':
-                    $nested = self::makeStringFromDiff($value1, $nextLevel);
-                    $stringifiedNest = implode("\n", $nested);
+                    $nested            = self::makeStringFromDiff($value1, $nextLevel);
+                    $stringifiedNest   = implode("\n", $nested);
                     $stringifiedDiff[] = "$spaces    $key: {\n$stringifiedNest\n$spaces    }";
                     break;
                 case 'unchanged':
@@ -45,46 +81,10 @@ class Stylish
                     $stringifiedValue1 = self::stringifyValue($value1, $nextLevel);
                     $stringifiedValue2 = self::stringifyValue($value2, $nextLevel);
                     $stringifiedDiff[] = "$spaces  - $key: $stringifiedValue1\n$spaces  + $key: $stringifiedValue2";
+                    break;
             }
         }
 
         return $stringifiedDiff;
-    }
-
-    private static function getSpaces(int $level): string
-    {
-        return str_repeat('    ', $level);
-    }
-
-    private static function stringifyValue(mixed $value, int $level): mixed
-    {
-        if (is_null($value)) {
-            return 'null';
-        } elseif (is_bool($value)) {
-            return $value ? 'true' : 'false';
-        } elseif (is_array($value)) {
-            $result = self::convertArrayToString($value, $level);
-            $spaces = self::getSpaces($level);
-            return "$result\n$spaces";
-        }
-
-        return "$value";
-    }
-
-    private static function convertArrayToString(array $value, int $level): string
-    {
-        $keys = array_keys($value);
-        $result = [];
-        $nextLevel = $level + 1;
-
-        $callback = function ($key) use ($value, $nextLevel) {
-            $newValue = self::stringifyValue($value[$key], $nextLevel);
-            $spaces = self::getSpaces($nextLevel);
-            return "\n{$spaces}{$key}: $newValue";
-        };
-
-        $result = array_map($callback, $keys);
-
-        return implode('', $result);
     }
 }
