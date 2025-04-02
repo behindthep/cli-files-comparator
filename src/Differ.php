9bc3e0c5cd5544dc0bdc2537ad7d69d663aedd2f
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-04-02 18:12:41 +0300

    fix visuability

--- src/Differ.php
+++ src/Differ.php
@@ -8,20 +8,18 @@ class Differ
 {
     public static function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
     {
-        $parsedFile1  = Parsers::parse($pathToFile1);
-        $parsedFile2  = Parsers::parse($pathToFile2);
-
+        $parsedFile1   = Parsers::parse($pathToFile1);
+        $parsedFile2   = Parsers::parse($pathToFile2);
         $difference    = self::makeDiff($parsedFile1, $parsedFile2);
         $formattedDiff = Formatters::makeFormat($difference, $format);
-
         return $formattedDiff;
     }
 
     private static function makeDiff(array $parsedFile1, array $parsedFile2): array
     {
         $uniqueKeys = self::getSortedUniqueKeys($parsedFile1, $parsedFile2);
-
-        return array_map(fn($key) => self::checkDifference($key, $parsedFile1, $parsedFile2), $uniqueKeys);
+        $difference = array_map(fn($key) => self::checkDifference($key, $parsedFile1, $parsedFile2), $uniqueKeys);
+        return $difference;
     }
 
     private static function checkDifference(mixed $key, array $parsedFile1, array $parsedFile2): array
@@ -69,15 +67,11 @@ class Differ
 
     private static function getSortedUniqueKeys(array $associativeArr1, array $associativeArr2): array
     {
-        $keys1 = array_keys($associativeArr1);
-        $keys2 = array_keys($associativeArr2);
-
+        $keys1        = array_keys($associativeArr1);
+        $keys2        = array_keys($associativeArr2);
         $combinedKeys = array_merge($keys1, $keys2);
         $uniqueKeys   = array_unique($combinedKeys);
-
-        /**
-         *
-         */
-        return fsort($uniqueKeys, fn($left, $right) => strcmp($left, $right));
+        $sortedKeys   = fsort($uniqueKeys, fn($left, $right) => strcmp($left, $right));
+        return $sortedKeys;
     }
 }
